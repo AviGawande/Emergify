42cd88114fa00b1c3458005e5896d1192c394d4d
Abhishek  Gawande <2041024@gcoej.ac.in>
2024-09-17 01:18:59 +0530

    add-walletprovider

---
+++ src/app/components/AppWalletProvider.js
@@ -0,0 +1,43 @@
+"use client";
+import React, { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+// Import the Solana wallet styles
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+export default function AppWalletProvider({ children }) {
+  // Select network, here Devnet is used
+  const network = WalletAdapterNetwork.Devnet;
+
+  // Set the network endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // Select wallets to be available in your dApp
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
